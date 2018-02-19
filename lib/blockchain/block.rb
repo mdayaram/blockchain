be4789682259901ce5df503e4065835f814ecdd9
Manoj Dayaram <noj@squareup.com>
2018-02-18 23:14:44 -0800

    serialization and deserialization of the blockchain

--- lib/blockchain/block.rb
+++ lib/blockchain/block.rb
@@ -25,5 +25,37 @@ module Blockchain
         nonce.next!
       end
     end
+
+    def ==(other)
+      return false if other.nil? || !other.is_a?(Block)
+
+      content == other.content &&
+        previous_hash == other.previous_hash &&
+        nonce == other.nonce
+    end
+
+    def to_json
+      {
+        previous_hash: previous_hash,
+        nonce: nonce,
+        content: content.respond_to?(:to_json) ? content.to_json : content.to_s
+      }
+    end
+
+    def self.from_json(b, content_class = nil)
+      block = Block.allocate # creates an instance without calling new/initialize
+      block.instance_variable_set(:@previous_hash, b[:previous_hash])
+      block.instance_variable_set(:@nonce, b[:nonce])
+
+      if content_class && content_class.respond_to?(:from_json)
+        block.instance_variable_set(:@content, content_class.from_json(b[:content]))
+      elsif content_class
+        block.instance_variable_set(:@content, content_class.new(b[:content]))
+      else
+        block.instance_variable_set(:@content, b[:content].to_s)
+      end
+
+      block
+    end
   end
 end
--- lib/blockchain/blockchain.rb
+++ lib/blockchain/blockchain.rb
@@ -11,9 +11,27 @@ module Blockchain
     end
 
     def valid?
-      @blocks.each_cons(2).all? do |a, b|
+      blocks.each_cons(2).all? do |a, b|
         a.block_hash == b.previous_hash
       end
+    end
+
+    def ==(other)
+      return false if other.nil? || !other.is_a?(Blockchain)
+
+      blocks == other.blocks
+    end
+
+    def to_json
+      blocks.map(&:to_json)
+    end
+
+    def self.from_json(json_blocks, content_class = nil)
+      blocks = json_blocks.map { |b| Block.from_json(b, content_class) }
+
+      blockchain = Blockchain.allocate # creates an instance without calling new/initialize
+      blockchain.instance_variable_set(:@blocks, blocks)
+      blockchain
     end
   end
 end
